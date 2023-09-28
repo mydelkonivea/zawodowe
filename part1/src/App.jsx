03e0268bc28be54fcd25647293f21198f4bd15d8
mydelkonivea <52548042+mydelkonivea@users.noreply.github.com>
2023-09-28 10:27:47 +0200

    test

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,4 @@
+const course = 'Half Stack application development'
 const part1 = 'Fundamentals of React'
 const exercises1 = 10
 const part2 = 'Using props to pass data'
@@ -7,8 +8,6 @@ const exercises3 = 14
 
 
 const App = () => {
-  // const-definition
-
   return (
     <div>
       <Header />
@@ -20,23 +19,26 @@ const App = () => {
 
 const Header = () => {
   return (
-    <p>
-    {part1} {exercises1}
-  </p>
+<h1>{course}</h1>
   )
 }
 const Content = () => {
   return (
-    <p>
+    <>    <p>
+    {part1} {exercises1}
+  </p>
+  <p>
     {part2} {exercises2}
   </p>
+  <p>
+    {part3} {exercises3}
+  </p></>
+
   )
 }
 const Total = () => {
   return (
-    <p>
-    {part3} {exercises3}
-  </p>
+<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
   )
 }
 
