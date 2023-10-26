131dad19f9bd306a63461aa8e4394d5afa412344
mydelkonivea <52548042+mydelkonivea@users.noreply.github.com>
2023-10-26 12:31:05 +0200

    Update App.jsx

--- part1.6-1.14/src/App.jsx
+++ part1.6-1.14/src/App.jsx
@@ -6,6 +6,73 @@ const Statistics = ({props, value}) => {
   )
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const MostVotes = ({points, dotes}) => {
+  var idBiggest =-1;
+
+  Object.keys(points).forEach(key => {
+    if(idBiggest < 0) idBiggest = key
+
+    if(points[idBiggest] < points[key]) idBiggest = key
+  
+  });
+
+
+    return(
+      <>
+      <h1>Anecdote with most votes</h1>
+      {dotes[idBiggest]}
+      <br/>
+      <p>has {points[idBiggest]} votes</p>
+      </>
+    )
+}
+
+const Parte = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const [points, setPoints] = useState({
+    0: 0, 1: 0,
+    2: 0, 3: 0,
+    4: 0, 5: 0,
+    6: 0, 7: 0,
+  })
+  
+  const [selected, setSelected] = useState(0)
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <br/>
+      has {points[selected]} votes
+      <br/>
+      <button onClick={() => {
+        
+        const newpoints = {...points}
+        newpoints[selected] += 1   
+        setPoints(newpoints)
+
+      }}>vote</button>
+      <button onClick={()=> {
+        setSelected(getRandomInt(anecdotes.length))
+      }}>netx anecdotes</button>
+
+      <MostVotes points = {points} dotes = {anecdotes}/>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -26,6 +93,9 @@ const App = () => {
       <Statistics props = "all" value = {bad + good + neutral}/>
       <Statistics props = "average" value = {(good + (bad*-1)) / (bad + good + neutral)}/>
       <Statistics props = "positive" value = {good/(bad + good + neutral)}/>
+
+      <br/>
+        <Parte/>
     </div>
   )
 }
